4d774a8ab12ea3d6ddac3ca9c11a2aa64ee76a17
DavidAnastasov <davidanastasov@hotmail.com>
2022-07-03 15:33:13 +0200

    Clean project and setup basic page with svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  base: "onvd-portfolio"
+  base: "onvd-portfolio",
 });
---
+++ src/components/Counter.svelte
@@ -0,0 +1,7 @@
+<script>
+    let count = 0;
+</script>
+
+<button on:click={() => (count -= 1)}>-</button>
+<span>{count}</span>
+<button on:click={() => (count += 1)}>+</button>
