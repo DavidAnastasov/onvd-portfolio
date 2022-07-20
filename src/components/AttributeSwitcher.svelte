edbfb9323435a5f26c98fa1e5b15fd35e0a90f60
DavidAnastasov <47048502+DavidAnastasov@users.noreply.github.com>
2022-07-20 17:43:36 +0200

    Latest version

---
+++ src/components/AttributeSwitcher.svelte
@@ -0,0 +1,54 @@
+<script>
+  const attributes = ["Full Stack Developer", "React.JS Developer", ".NET Developer"];
+
+  let currentAttribute = attributes[0];
+  let currentIndex = 0;
+
+  let displayedWord = "";
+
+  function decreaseLetters() {
+    if (displayedWord.length === 0) {
+      setTimeout(() => {
+        if (currentIndex + 1 > attributes.length - 1) {
+          currentIndex = 0;
+        } else {
+          currentIndex += 1;
+        }
+
+        currentAttribute = attributes[currentIndex];
+      }, 1000);
+      return;
+    }
+
+    displayedWord = displayedWord.substring(0, displayedWord.length - 1) ?? "";
+    setTimeout(decreaseLetters, 50);
+  }
+
+  function increaseLetters(letters = "") {
+    if (letters.length === 0) {
+      setTimeout(decreaseLetters, 1500);
+      return;
+    }
+
+    displayedWord += letters[0];
+    setTimeout(() => increaseLetters(letters.substring(1, letters.length)), 75);
+  }
+
+  $: {
+    increaseLetters(currentAttribute + "...");
+  }
+</script>
+
+<p>
+  {#if displayedWord.length !== 0}
+    {displayedWord}
+  {:else}
+    &nbsp;
+  {/if}
+</p>
+
+<style>
+  P {
+    font-size: 1.2rem;
+  }
+</style>
